72cff13aec5941c1d26348ab84de5a45d870dfa3
Fedor <porotnikov.fiodor@yandex.ru>
2020-05-27 00:10:06 +0500

    survey design

--- src/component/Survey/NewSurvey/NewSurvey.jsx
+++ src/component/Survey/NewSurvey/NewSurvey.jsx
@@ -12,8 +12,11 @@ const NewSurvey = () => {
             </div>
             <div className={style['content']}>
                 <div className={style['data']}>23.06.2020 14:54{/* время публикации опроса */}</div>
-                <div className={style['theme']}>Тема: <input type="text" value=""/></div>
+                <div className={style['theme']}>Тема: <input type="text" /></div>
                 <button className={style['option']}><i class="fas fa-plus"></i>Добавить вариант</button>
+                <div className={style['variant']}>
+                    <input className={style['add-option']} type="text" placeholder='Добавить вариант' />
+                </div>
             </div>
         </div>
     );
