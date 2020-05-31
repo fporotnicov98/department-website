7f66e5aacc8191f76334ffc1ca00100652f07258
Fedor <porotnikov.fiodor@yandex.ru>
2020-05-31 19:55:18 +0500

    Updates

---
+++ precache-manifest.12f2557f290d3df72c67f47921df68c4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847bbbf289855beb87d4b791272b6310",
+    "url": "/department-website/index.html"
+  },
+  {
+    "revision": "a259eeae9d1c84ed15e6",
+    "url": "/department-website/static/css/2.b18f5fde.chunk.css"
+  },
+  {
+    "revision": "66747f64029973efbb0a",
+    "url": "/department-website/static/css/main.e19c3615.chunk.css"
+  },
+  {
+    "revision": "a259eeae9d1c84ed15e6",
+    "url": "/department-website/static/js/2.d425d1d6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/department-website/static/js/2.d425d1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66747f64029973efbb0a",
+    "url": "/department-website/static/js/main.53774150.chunk.js"
+  },
+  {
+    "revision": "29a6d80cf49939004324",
+    "url": "/department-website/static/js/runtime-main.0a504879.js"
+  },
+  {
+    "revision": "5a4a0dd78b70e1ceef35f46f9bc9faa6",
+    "url": "/department-website/static/media/1.5a4a0dd7.jpg"
+  },
+  {
+    "revision": "bb373e31b5a5836d0ce1bf320ef7afca",
+    "url": "/department-website/static/media/2.bb373e31.jpg"
+  },
+  {
+    "revision": "74443d332a3e6e8444751841506c76e4",
+    "url": "/department-website/static/media/3.74443d33.jpg"
+  },
+  {
+    "revision": "f0c06e73848a7f2df93c8ce90c22b5e3",
+    "url": "/department-website/static/media/4.f0c06e73.jpg"
+  },
+  {
+    "revision": "20473e1c530bf9a1952ec555c7890659",
+    "url": "/department-website/static/media/5.20473e1c.jpg"
+  },
+  {
+    "revision": "27ab2d2ea3e7fe65921d52a3b84e7bca",
+    "url": "/department-website/static/media/down.27ab2d2e.svg"
+  },
+  {
+    "revision": "b8f80926c10054ac5a936f7beaab0e94",
+    "url": "/department-website/static/media/logo.b8f80926.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/department-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/department-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/department-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/department-website/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c33a6e6f8d6496988cc74a908d324ee7",
+    "url": "/department-website/static/media/up.c33a6e6f.svg"
+  }
+]);
