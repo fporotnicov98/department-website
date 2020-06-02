73da5ca6aed8db55aa41849f546ac648c2e4b03c
Fedor <porotnikov.fiodor@yandex.ru>
2020-06-02 14:24:06 +0500

    Updates

--- precache-manifest.d1f77cbfc8ece7f3add1c87f4b703e6f.js
+++ precache-manifest.d1f77cbfc8ece7f3add1c87f4b703e6f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d517b20137ba4affe6fa8dcf8b976311",
+    "revision": "d129ff03ac9053d756d6e709bf68cd1c",
     "url": "/department-website/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/department-website/static/css/2.b18f5fde.chunk.css"
   },
   {
-    "revision": "6e5fe76056efa63e5352",
-    "url": "/department-website/static/css/main.e914b1fa.chunk.css"
+    "revision": "5407d56b05ddcce97991",
+    "url": "/department-website/static/css/main.945ecb81.chunk.css"
   },
   {
     "revision": "eeb9855a3359c34a20ff",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/department-website/static/js/2.3ef7204f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6e5fe76056efa63e5352",
-    "url": "/department-website/static/js/main.4fb0da56.chunk.js"
+    "revision": "5407d56b05ddcce97991",
+    "url": "/department-website/static/js/main.dafab9ac.chunk.js"
   },
   {
     "revision": "29a6d80cf49939004324",
